2a78c22d9e925b4fe43d17bf5c72496fea911639
Alicia <A.Ireland2@newcastle.ac.uk>
2023-10-17 14:41:53 +0100

    Added to views.py to add new users to db.

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
+from models import User
 from users.forms import RegisterForm
+from app import db
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
 
@@ -8,10 +10,15 @@ def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        # print statements to check functionality
-        print(form.data.get('username'))
-        print(form.data.get('password'))
-        print(form.data.get('confirmPassword'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            flash("Username already exists")
+            return render_template('users/register.html', form=form)
+
+        new_user = User(username=form.username.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+
         return redirect(url_for('users.login'))
 
     return render_template('users/register.html', form=form)
